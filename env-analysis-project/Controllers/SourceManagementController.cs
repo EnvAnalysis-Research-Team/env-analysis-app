91fb977d9fda976bc52138aa746103ca1dc5e7c3
KhanhNguyenVimaru <khanhnd05@gmail.com>
2025-10-24 15:42:01 +0700

    CRUD emission source UI

--- env-analysis-project/Controllers/SourceManagementController.cs
+++ env-analysis-project/Controllers/SourceManagementController.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using env_analysis_project.Data;
 
 namespace env_analysis_project.Controllers
 {
     public class SourceManagementController : Controller
     {
+        private readonly env_analysis_projectContext _context;
+
+        public SourceManagementController(env_analysis_projectContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Manage()
+
+        // Serve Manage view with emission sources model and source types (used by the sidebar)
+        public async Task<IActionResult> Manage()
         {
-            return View("Manage");
+            var sources = await _context.EmissionSource
+                .Include(e => e.SourceType)
+                .OrderBy(e => e.SourceName)
+                .ToListAsync();
+
+            var sourceTypes = await _context.SourceType
+                .Select(st => new
+                {
+                    st.SourceTypeID,
+                    st.SourceTypeName,
+                    st.Description,
+                    st.IsActive,
+                    st.CreatedAt,
+                    st.UpdatedAt,
+                    Count = _context.EmissionSource.Count(es => es.SourceTypeID == st.SourceTypeID)
+                })
+                .ToListAsync();
+
+            ViewBag.SourceTypes = sourceTypes;
+            return View("Manage", sources);
         }
     }
 }
